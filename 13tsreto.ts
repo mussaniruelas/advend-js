2a8cc671b6a33c0fde9e7a14e46ba76fbb73b577
Mussani Ruelas <mussaniruelas222@gmail.com>
2025-01-09 10:35:47 -0400

    subiendo reto 13 en ts

---
+++ 13tsreto.ts
@@ -0,0 +1,26 @@
+function minMovesToStables(reindeer: number[], stables: number[]): number {
+  reindeer.sort();
+  stables.sort();
+
+  return reindeer.reduce((s, t, i) => {
+    return s + Math.abs(t - stables[i]);
+  }, 0);
+}
+
+console.log(minMovesToStables([2, 6, 9], [3, 8, 5])); // -> 3
+// Explicación:
+// Renos en posiciones: 2, 6, 9
+// Establos en posiciones: 3, 8, 5
+// 1er reno: se mueve de la posición 2 al establo en la posición 3 (1 movimiento).
+// 2do reno: se mueve de la posición 6 al establo en la posición 5 (1 movimiento)
+// 3er reno: se mueve de la posición 9 al establo en la posición 8 (1 movimiento).
+// Total de movimientos: 1 + 1 + 1 = 3 movimientos
+
+console.log(minMovesToStables([1, 1, 3], [1, 8, 4]));
+// Explicación:
+// Renos en posiciones: 1, 1, 3
+// Establos en posiciones: 1, 8, 4
+// 1er reno: no se mueve (0 movimientos)
+// 2do reno: se mueve de la posición 1 al establo en la posición 4 (3 movimientos)
+// 3er reno: se mueve de la posición 3 al establo en la posición 8 (5 movimientos)
+// Total de movimientos: 0 + 3 + 5 = 8 movimientos
